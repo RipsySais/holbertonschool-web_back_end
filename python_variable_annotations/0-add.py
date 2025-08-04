e3f8348eb669ac1ca9c92073c6bed38d0b6252d2
RipsySais <cysseahmed@gmail.com>
2025-08-04 19:03:16 +0000

    0-basic

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Attend un délai aléatoire entre 0 et max_delay secondes (inclus)
+    et retourne ce délai.
+    
+    Args:
+        max_delay (int): Le délai maximum (par défaut 10)
+        
+    Returns:
+        float: Le délai aléatoire qui a été attendu
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
