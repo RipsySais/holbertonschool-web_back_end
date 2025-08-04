2f3938e5869f2dac0730d4438c7d5ac6af3dd3b0
RipsySais <cysseahmed@gmail.com>
2025-08-04 18:49:53 +0000

    0-basic et 0-main

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-"""Async coroutine that waits for a random delay."""
-
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay (included).
-    Returns:
-        float: the actual delay waited.
-    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
