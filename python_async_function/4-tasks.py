2779aeed4eb889b535e3172639ee3cdafde6a385
RispsySais <cysseahmed@gmail.com>
2025-08-05 03:12:33 +0200

    Add task_wait_n function to create multiple asyncio tasks concurrently

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module for creating multiple asyncio tasks concurrently.
+"""
+
+import asyncio
+
+# Import task_wait_random from the previous file
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    Spawn task_wait_random n times with max_delay.
+    Return list of delays in ascending order (order of completion).
+    
+    Args:
+        n (int): Number of tasks to spawn
+        max_delay (int): Maximum delay for each task
+        
+    Returns:
+        list[float]: List of delays in ascending order
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    results = []
+    for task in asyncio.as_completed(tasks):
+        res = await task
+        results.append(res)
+
+    return results
