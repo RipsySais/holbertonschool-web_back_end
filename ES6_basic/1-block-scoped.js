1bd3de5fb47086a1c86de7787e4d2018321936a4
RipsySais <cysseahmed@gmail.com>
2025-08-20 10:19:21 +0000

    1-block

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = true;
-    const innerTask2 = false;
-    // Les variables internes n'affectent pas les externes
+    const task = true;    // Nouvelle variable de bloc
+    const task2 = false;  // Nouvelle variable de bloc
   }
 
   return [task, task2];
