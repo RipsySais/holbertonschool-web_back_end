237810fd46a2e8b26aa5c9e075d5a0470ae83fb4
RipsySais <cysseahmed@gmail.com>
2025-08-20 09:48:58 +0000

    0-constants

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,12 @@
-// 0-constants.js
-
-// Utilise const car la valeur ne change pas
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Fonction fournie
 export function getLast() {
   return ' is okay';
 }
 
-// Utilise let car la valeur est modifiée
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
