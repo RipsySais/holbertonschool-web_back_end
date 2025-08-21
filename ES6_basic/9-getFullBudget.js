29c619f9f3d4d65e78afe933bab472faeddc3a74
RipsySais <cysseahmed@gmail.com>
2025-08-21 00:43:47 +0000

    9-getFullBudget

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,8 +1,16 @@
-// Exemple de syntaxe méthode raccourcie
-const obj = {
-  // Ancienne façon
-  oldMethod: function() { return 'old'; },
-  
-  // Nouvelle façon ES6
-  newMethod() { return 'new'; }
-};
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
