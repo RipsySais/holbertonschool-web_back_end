324d0da27570218982b803f6c045720b69b64964
RipsySais <cysseahmed@gmail.com>
2025-08-25 22:13:08 +0000

    tous les fichiers

---
+++ ES6_basic/jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  testMatch: [
+    "**/*.test.js"   // dit à Jest de prendre tous les fichiers terminant par .test.js
+  ],
+  transform: {},     // évite les erreurs inutiles avec Babel si non nécessaire
+};
