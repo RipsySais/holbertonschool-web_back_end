fbf98062815be2b9df563aa747a9cac119c19b63
RipsySais <cysseahmed@gmail.com>
2025-08-25 23:10:35 +0000

    jest

--- ES6_basic/jest.config.cjs
+++ ES6_basic/jest.config.cjs
@@ -1,7 +1,6 @@
-export default {
+module.exports = {
   testEnvironment: "node",
   transform: {},
-  testMatch: ["**/tests/**/*.test.js"],  // tests au lieu de __tests__
-  moduleFileExtensions: ["js", "json"],
-  extensionsToTreatAsEsm: ['.js']
+  testMatch: ["**/tests/**/*.test.js"],
+  moduleFileExtensions: ["js", "json"]
 };
