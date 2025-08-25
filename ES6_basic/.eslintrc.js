1f0a19626c0d02224cbac17eff26e47596a00c42
RipsySais <cysseahmed@gmail.com>
2025-08-25 21:22:31 +0000

    tous les fichires

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
+    node: true,      
     es2021: true,
     jest: true,
   },
   extends: 'eslint:recommended',
-  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
+    semi: ['error', 'always'],   
+    quotes: ['error', 'double'], 
   },
 };
