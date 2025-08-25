636831b86de0ba7cc34a9101e3cb69eeff09f26d
RipsySais <cysseahmed@gmail.com>
2025-08-25 22:40:58 +0000

    jest

--- ES6_basic/jest.config.js
+++ ES6_basic/jest.config.js
@@ -1,6 +1,7 @@
 // jest.config.js
 export default {
-  testEnvironment: "node",             // environnement Node
-  transform: {},                        // pas de Babel pour tests simples
-  testMatch: ["**/__tests__/**/*.test.js"] // tous les fichiers .test.js dans __tests__
+  testEnvironment: "node",
+  transform: {},
+  testMatch: ["**/__tests__/**/*.test.js"], // tous les fichiers .test.js dans __tests__
+  moduleFileExtensions: ["js", "json"]
 };
