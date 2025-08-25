d0cd0ead756214693b868a7a8b456d78de830c86
RipsySais <cysseahmed@gmail.com>
2025-08-25 23:59:14 +0000

    0-constants

--- ES6_basic/tests/0-constants.test.js
+++ ES6_basic/tests/0-constants.test.js
@@ -1,9 +1,11 @@
 import { taskFirst, taskNext } from "../0-constants.js";
 
-test("taskFirst returns correct string", () => {
-  expect(taskFirst()).toBe("I prefer const when I can.");
-});
+describe("Test suite for 0-constants.js", () => {
+  test("taskFirst returns correct string", () => {
+    expect(taskFirst()).toBe("I prefer const when I can.");
+  });
 
-test("taskNext returns correct combination string", () => {
-  expect(taskNext()).toBe("But sometimes let is okay");
+  test("taskNext returns correct combination string", () => {
+    expect(taskNext()).toBe("But sometimes let is okay");
+  });
 });
