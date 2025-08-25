f70e751be6351a80f4cb911edb901be5d2abb309
RipsySais <cysseahmed@gmail.com>
2025-08-25 22:21:35 +0000

    jest

--- ES6_basic/jest.config.js
+++ ES6_basic/jest.config.js
@@ -1,7 +1,5 @@
 export default {
-  testEnvironment: "node",            // environnement Node
-  extensionsToTreatAsEsm: [".js"],    // traite tous les .js comme ESM
-  transform: {},                       // pas de transformation Babel nécessaire pour tests simples
-  testMatch: ["**/*.test.js"],         // tous les fichiers .test.js, même à la racine
-  moduleFileExtensions: ["js", "json"] // extensions reconnues
+  testEnvironment: "node",       // environnement Node
+  transform: {},                  // pas de transformation Babel pour tests simples
+  testMatch: ["**/*.test.js"]     // tous les fichiers .test.js
 };
