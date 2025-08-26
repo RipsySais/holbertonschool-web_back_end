8a3c65c09b3f9e56f9caeef7663eaea02dd69b5d
RipsySais <cysseahmed@gmail.com>
2025-08-26 23:33:36 +0000

    0-classroom

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,4 +1,11 @@
+// 0-classroom.js
+
+// Définition de la classe ClassRoom et export par défaut
 export default class ClassRoom {
+  /**
+   * Constructeur de la classe
+   * @param {Number} maxStudentsSize - nombre maximum d'étudiants
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
