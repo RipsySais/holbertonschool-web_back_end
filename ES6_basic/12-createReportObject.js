379ceeca7e465a4ef7a76f1509e2a35918b36290
RipsySais <cysseahmed@gmail.com>
2025-09-01 07:51:48 +0000

    11-create

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,15 @@
-export default function createReportObject(employeesList) {
+// 11-createEmployeesObject.js
+
+/**
+ * Creates an object with the department name as key
+ * and an array of employees as value.
+ *
+ * @param {string} departmentName - The name of the department
+ * @param {string[]} employees - The list of employees
+ * @returns {Object} - Object with departmentName as key and employees as value
+ */
+export default function createEmployeesObject(departmentName, employees) {
   return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments(allEmployees) {
-      return Object.keys(allEmployees).length;
-    },
+    [departmentName]: employees,
   };
 }
