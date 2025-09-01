3914b49370230fbc6c566bb2181e8a187a91be50
RipsySais <cysseahmed@gmail.com>
2025-09-01 07:53:47 +0000

    11-create

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,15 @@
+// 11-createEmployeesObject.js
+
+/**
+ * Creates an object with the department name as key
+ * and an array of employees as value.
+ *
+ * @param {string} departmentName - The name of the department
+ * @param {string[]} employees - The list of employees
+ * @returns {Object} - Object with departmentName as key and employees as value
+ */
 export default function createEmployeesObject(departmentName, employees) {
   return {
-    [departmentName]: employees
+    [departmentName]: employees,
   };
 }
