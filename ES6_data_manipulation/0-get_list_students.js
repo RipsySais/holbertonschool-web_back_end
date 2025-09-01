fd7d74af1a0b6e965442e763ef7dae6a2bc04c35
RipsySais <cysseahmed@gmail.com>
2025-09-01 10:16:10 +0000

    0. Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,9 @@
-// 0-get_list_students.js
-
-/**
- * Returns an array of student objects.
- * Each object contains: id (Number), firstName (String), location (String).
- *
- * @returns {Array} List of students
- */
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+function getListStudents() {
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
 }
+
+export default getListStudents;
