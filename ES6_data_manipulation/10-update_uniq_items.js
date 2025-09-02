3b18aa349d4d34ed6fca71c43b7267091b35887a
RipsySais <cysseahmed@gmail.com>
2025-09-02 21:42:10 +0000

    Ajoute la fonction updateUniqueItems pour mettre à jour les quantités uniques dans une Map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,16 @@
-function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-        throw new Error('Cannot process');
-    }
-    
-    for (const [item, quantity] of map) {
-        if (quantity === 1) {
-            map.set(item, 100);
-        }
-    }
-    
-    return map;
-}
+/**
+ * Met à jour les quantités des articles dont la quantité initiale est 1 dans une Map.
+ * @param {Map} map - La Map à mettre à jour.
+ * @throws {Error} - Si l'argument n'est pas une Map.
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-export default updateUniqueItems;
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+}
