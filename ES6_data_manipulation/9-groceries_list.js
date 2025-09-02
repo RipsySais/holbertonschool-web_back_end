a6b9fd8fcb3772a10c708b591bc2b927d4da6166
RipsySais <cysseahmed@gmail.com>
2025-09-02 21:38:17 +0000

    Ajoute la fonction groceriesList pour créer une Map de courses

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,13 @@
-function groceriesList() {
-    const groceries = new Map();
-    
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Banana', 5);
-    
-    return groceries;
+/**
+ * Retourne une Map contenant une liste de courses avec leurs quantités.
+ * @returns {Map} - Une Map avec les noms des articles comme clés et les quantités comme valeurs.
+ */
+export default function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
 }
-
-export default groceriesList;
