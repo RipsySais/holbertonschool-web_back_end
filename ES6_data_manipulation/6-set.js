eac822f78173826f6435e76c7b27220d7c89b618
RipsySais <cysseahmed@gmail.com>
2025-09-02 21:28:18 +0000

    Ajoute la fonction setFromArray pour créer un Set à partir d'un tableau

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,5 +1,8 @@
-function setFromArray(array) {
-    return new Set(array);
+/**
+ * Crée un Set à partir d'un tableau.
+ * @param {Array} array - Tableau d'éléments de n'importe quel type.
+ * @returns {Set} - Un Set contenant les éléments uniques du tableau.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
 }
-
-export default setFromArray;
