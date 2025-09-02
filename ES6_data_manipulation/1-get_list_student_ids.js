cfaa3df7eb8c2808d93febc75a7732d759d99b60
RipsySais <cysseahmed@gmail.com>
2025-09-02 07:47:49 +0000

    Ajoute la fonction getListStudentIds pour extraire les IDs des étudiants

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,11 @@
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+/**
+ * Retourne un tableau d'IDs d'étudiants à partir d'une liste d'objets.
+ * @param {Array} students - Tableau d'objets étudiants.
+ * @returns {Array} - Tableau d'IDs ou un tableau vide si l'argument n'est pas un tableau.
+ */
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 }
-
-export default getListStudentIds;
