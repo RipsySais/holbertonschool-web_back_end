3cc8bcf426d1225efb649f9d6d84b41b4adeedf5
RipsySais <cysseahmed@gmail.com>
2025-09-02 21:31:58 +0000

    Ajoute la fonction hasValuesFromArray pour vérifier si tous les éléments d'un tableau sont dans un Set

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,9 @@
-function hasValuesFromArray(set, array) {
-    if (!(set instanceof Set) || !Array.isArray(array)) {
-        return false;
-    }
-    
-    return array.every(element => set.has(element));
+/**
+ * Vérifie si tous les éléments d'un tableau existent dans un Set.
+ * @param {Set} set - Le Set à vérifier.
+ * @param {Array} array - Le tableau d'éléments à vérifier.
+ * @returns {Boolean} - `true` si tous les éléments du tableau sont dans le Set, sinon `false`.
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
 }
-
-export default hasValuesFromArray;
