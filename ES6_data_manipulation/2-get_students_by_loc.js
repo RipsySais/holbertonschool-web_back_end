93b471d58440836c82382597aae13bab43244589
RipsySais <cysseahmed@gmail.com>
2025-09-02 07:51:08 +0000

    Ajoute la fonction getStudentsByLocation pour filtrer les étudiants par ville

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,9 @@
-function getStudentsByLocation(students, city) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.filter(student => student.location === city);
+/**
+ * Retourne un tableau d'étudiants situés dans une ville spécifique.
+ * @param {Array} students - Tableau d'objets étudiants.
+ * @param {String} city - Ville à filtrer.
+ * @returns {Array} - Tableau d'étudiants filtrés.
+ */
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
-
-export default getStudentsByLocation;
