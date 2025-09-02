6f1998c4d5c5bbebe4e7fddcf5d9f4c12f446ef2
RipsySais <cysseahmed@gmail.com>
2025-09-02 22:10:44 +0000

    Ajoute la fonction getResponseFromAPI qui retourne une Promise et configure Babel

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+/**
+ * Retourne une Promise.
+ * @returns {Promise} - Une Promise.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // La Promise est créée, mais elle n'est ni résolue ni rejetée ici.
+    // Cela permet de vérifier que la fonction retourne bien une Promise.
+  });
+}
