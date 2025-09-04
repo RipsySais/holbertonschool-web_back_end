2a3538134e0e206100e807afaa61667ebcd0cbd1
RipsySais <cysseahmed@gmail.com>
2025-09-04 15:07:02 +0000

    update task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
 
-// Lorsqu'on reçoit une donnée sur stdin
+// Traite la saisie
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${data.trim()}\n`);
 });
 
-// Lorsqu'on termine stdin (ex: echo "John" | node 1-stdin.js, ou Ctrl+D)
+// Gère la fin d'entrée (Ctrl+D ou fin du pipe)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
 
-// Nécessaire pour le mode pipe
 process.stdin.resume();
