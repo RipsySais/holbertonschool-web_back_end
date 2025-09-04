b0a1b63a137dae91243bf94327881101400a5c98
RipsySais <cysseahmed@gmail.com>
2025-09-04 09:38:29 +0000

    Add 0-console.js - function displayMessage

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+// 0-console.js
+
+/**
+ * Fonction qui affiche un message dans la sortie standard
+ * @param {string} message - Le message à afficher
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+// Exportation de la fonction pour qu'elle puisse être utilisée ailleurs
+module.exports = displayMessage;
