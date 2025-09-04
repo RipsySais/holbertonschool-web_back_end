a939b4c78bb02f2bd573fdb6fdc175db5d30cef5
RipsySais <cysseahmed@gmail.com>
2025-09-04 09:56:23 +0000

    Add 1-stdin.js - program using process.stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,3 @@
-// 1-stdin.js
-
-/**
- * Programme qui demande le nom de l'utilisateur et l'affiche
- */
-
 // Affiche le message d'accueil
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
@@ -13,7 +7,10 @@ process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-// Lorsque le programme se termine (Ctrl+D ou pipe terminé)
-process.on("exit", () => {
-  console.log("This important software is now closing");
+// Écoute la fin de l'entrée (par exemple, Ctrl+D ou fin de l'entrée pipée)
+process.stdin.on('end', () => {
+  process.stdout.write("This important software is now closing\n");
 });
+
+// Résume le processus stdin pour recevoir les entrées
+process.stdin.resume();
