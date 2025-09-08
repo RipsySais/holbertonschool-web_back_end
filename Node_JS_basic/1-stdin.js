acc8bb7bb91be7bbd247aab3b3e4437c51b53520
RipsySais <cysseahmed@gmail.com>
2025-09-08 13:10:25 +0000

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
 process.stdin.setEncoding('utf8');
 
 // Traite la saisie
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.trim()}\n`);
+  process.stdout.write(`Your name is: ${data.trim()}\r\n`);
 });
 
 // Gère la fin d'entrée (Ctrl+D ou fin du pipe)
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\r\n');
 });
 
 process.stdin.resume();
+
